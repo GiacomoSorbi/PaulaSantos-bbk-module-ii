a5c6d68f58a88ee3e36c35f6dc6b40d8f7ee9b12
unknown <pangeriz@outlook.com>
2020-07-05 10:30:39 +0100

    Updates

---
+++ precache-manifest.ebe2144003e012eb6a7f66b9ab8257fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306d18ba90eb107c214a9d789a7ccff4",
+    "url": "/PaulaSantos-bbk-module-ii/index.html"
+  },
+  {
+    "revision": "887d710968064c7114bd",
+    "url": "/PaulaSantos-bbk-module-ii/static/css/main.d8c98579.chunk.css"
+  },
+  {
+    "revision": "daca026b5c2c11ec627a",
+    "url": "/PaulaSantos-bbk-module-ii/static/js/2.6797346c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PaulaSantos-bbk-module-ii/static/js/2.6797346c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887d710968064c7114bd",
+    "url": "/PaulaSantos-bbk-module-ii/static/js/main.8649c44a.chunk.js"
+  },
+  {
+    "revision": "28aa5b03c279c8a3722f",
+    "url": "/PaulaSantos-bbk-module-ii/static/js/runtime-main.137bf9fb.js"
+  },
+  {
+    "revision": "21fc9d9a4f81a1d0a528420044734470",
+    "url": "/PaulaSantos-bbk-module-ii/static/media/starter.21fc9d9a.jpg"
+  }
+]);
