0e2ab8f1a2ad374a322e9ff29cf396e1016d3435
unknown <pangeriz@outlook.com>
2020-07-11 22:06:47 +0100

    test

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -9,23 +9,23 @@ const HomePage = () => (
 
     <div className="container-1">
       <a className="link" href="/starter">
-        <h2 className="h2">Olivier salad</h2>
+        <h2>Olivier salad</h2>
       </a>
     </div>
 
     <div className="container-2">
       <a className="link" href="/main">
-        <h2 className="h2">Meatballs in tomato sauce</h2>
+        <h2>Meatballs in tomato sauce</h2>
       </a>
     </div>
     <div className="container-3">
       <a className="link" href="/dessert">
-        <h2 className="h2">Walnut and chocolate cake</h2>
+        <h2>Walnut and chocolate cake</h2>
       </a>
     </div>
     <div className="container-4">
       <a className="link" href="/pies">
-        <h2 className="h2">Tuna pie</h2>
+        <h2>Tuna pie</h2>
       </a>
     </div>
   </div>
