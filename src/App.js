01e009ec91e92d3013dfed6a6aa37556fe45eafb
unknown <pangeriz@outlook.com>
2020-07-12 13:25:10 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import {
   HomePage,
   StarterPage,
