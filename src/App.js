e600ea8e9548c0f0ad0f7b1d5ba6562e4d27e093
unknown <pangeriz@outlook.com>
2020-07-12 15:23:36 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import {
   HomePage,
   StarterPage,
@@ -14,17 +14,15 @@ import {
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/starter" component={StarterPage} />
-        <Route path="/main" component={MainPage} />
-        <Route path="/dessert" component={DessertPage} />
-        <Route path="/pies" component={PiesPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/visual" component={VisualPage} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/starter" component={StarterPage} />
+      <Route path="/main" component={MainPage} />
+      <Route path="/dessert" component={DessertPage} />
+      <Route path="/pies" component={PiesPage} />
+      <Route path="/contact" component={ContactPage} />
+      <Route path="/visual" component={VisualPage} />
+    </Switch>
 
     /*
     <div className="App">
