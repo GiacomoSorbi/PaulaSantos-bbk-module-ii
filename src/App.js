e0c37afd9c775e6baea96a422c833f58a28bc7f0
unknown <pangeriz@outlook.com>
2020-07-13 20:48:35 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Route, Switch } from "react-router-dom";
 import {
   HomePage,
   StarterPage,
@@ -15,62 +15,43 @@ import {
 function App() {
   return (
     <Switch>
-      <Router
+      <Route
         basename={process.env.PUBLIC_URL}
         path="/"
         exact
         component={HomePage}
       />
-      <Router
+      <Route
         basename={process.env.PUBLIC_URL}
         path="/starter"
         component={StarterPage}
       />
-      <Router
+      <Route
         basename={process.env.PUBLIC_URL}
         path="/main"
         component={MainPage}
       />
-      <Router
+      <Route
         basename={process.env.PUBLIC_URL}
         path="/dessert"
         component={DessertPage}
       />
-      <Router
+      <Route
         basename={process.env.PUBLIC_URL}
         path="/pies"
         component={PiesPage}
       />
-      <Router
+      <Route
         basename={process.env.PUBLIC_URL}
         path="/contact"
         component={ContactPage}
       />
-      <Router
+      <Route
         basename={process.env.PUBLIC_URL}
         path="/visual"
         component={VisualPage}
       />
     </Switch>
-
-    /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> 
-    */
   );
 }
 
