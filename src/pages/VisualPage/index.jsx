184d7af417b54ae463b335ee44e1883f94169934
unknown <pangeriz@outlook.com>
2020-07-14 19:40:40 +0100

    assets

--- src/pages/VisualPage/index.jsx
+++ src/pages/VisualPage/index.jsx
@@ -5,6 +5,72 @@ const VisualPage = () => (
   <div className="container">
     <div className="content">
       <h1 className="title">Visual design</h1>
+      <h2>Image 1 - Homepage hero</h2>
+      <h3>Original</h3>
+      <img src={require("../assets/home-original.jpg")} alt="Homemade bread" />
+      <p>[description]</p>
+      <h3>Edited version</h3>
+      <img src={require("../assets/home.jpg")} alt="Homemade bread" />
+      <p>[description]</p>
+      <h2>Image 2 - Starter</h2>
+
+      <h3>Original</h3>
+      <img
+        src={require("../assets/starter-original.jpg")}
+        alt="Olivier salad"
+      />
+      <p>[description]</p>
+      <h3>Edited version</h3>
+      <img src={require("../assets/starter.jpg")} alt="Olivier salad" />
+      <p>[description]</p>
+      <h2>Image 3 - Main course</h2>
+      <h3>Original</h3>
+      <img
+        src={require("../assets/main-original.jpg")}
+        alt="Meatballs in tomato sauce"
+      />
+      <p>[description]</p>
+      <h3>Edited version</h3>
+      <img
+        src={require("../assets/main.jpg")}
+        alt="Meatballs in tomato sauce"
+      />
+      <p>[description]</p>
+      <h2>Image 4 - Dessert</h2>
+      <h3>Original</h3>
+      <img
+        src={require("../assets/dessert-original.jpg")}
+        alt="Walnut and chocolate cake"
+      />
+      <p>[description]</p>
+      <h3>Edited version</h3>
+      <img
+        src={require("../assets/dessert.jpg")}
+        alt="Walnut and chocolate cake"
+      />
+      <p>[description]</p>
+      <h2>Image 5 - Pie</h2>
+      <h3>Original</h3>
+      <img src={require("../assets/pie-original.jpg")} alt="Tuna pie" />
+      <p>[description]</p>
+      <h3>Edited version</h3>
+      <img src={require("../assets/pie.jpg")} alt="Tuna pie" />
+      <p>[description]</p>
+      <h2>Image 6 - Kitchen vector illustration</h2>
+      <h3>Original</h3>
+      <img
+        src={require("../assets/kitchen-vector-original.jpg")}
+        alt="Kitchen vector illustration"
+      />
+      <p>[description]</p>
+      <h3>Edited version</h3>
+      <img
+        src={require("../assets/kitchen-vector.png")}
+        alt="Kitchen vector illustration"
+      />
+      <p>[description]</p>
+      <h2>Animated GIF</h2>
+      <p>[description]</p>
     </div>
   </div>
 );
