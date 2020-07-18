47894b21a43c0cf22e5924f74ce288c0dde67876
unknown <pangeriz@outlook.com>
2020-07-18 18:20:18 +0100

    deploy

--- src/pages/StarterPage/index.jsx
+++ src/pages/StarterPage/index.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles.css";
 import ShoppingList from "./ShoppingList";
 
-const StarterPage = () => (
-  <div>
-    <ShoppingList />
-  </div>
-);
+const StarterPage = () => {
+  const [lists] = useState([]);
+
+  return (
+    <div>
+      <ShoppingList lists={lists} />
+    </div>
+  );
+};
 
 export default StarterPage;
